64ea2c66275c9e6bff8c2c6385baa2c32bb8927f
Luis Antonio <luis.developer.ac@gmail.com>
2021-10-13 19:03:44 -0500

    questao 5

--- main.java
+++ main.java
@@ -2,12 +2,10 @@ class Main {
 
   public static void main(String[] args) {
     Conta c1 = new Conta();
-
-    c1.titular = "Danilo";
+    c1.titular = "Hugo";
     c1.saldo = 100;
-    Conta c2 = new Conta();
-    c2.titular = "Danilo";
-    c2.saldo = 100;
+    Conta c2 = c1;
+
     if (c1 == c2) {
       System.out.println("iguais");
     } else {
